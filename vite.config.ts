b406e7887b4b427902332fe9a5090c6ba5ee1252
Yen-Chun,Lin <asd900606@gmail.com>
2025-12-30 21:45:23 +0800

    chore: deploy on github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Bulls-and-cows-ASL/",
   resolve: {
     alias: {
       "@": "/src",
